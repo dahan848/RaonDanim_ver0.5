9c88f31018ff9ed34249bba6b427c602e33ea050
manzza0810 <manzza0810@gmail.com>
2019-03-21 18:09:26 +0900

    회원 : 회원가입 요청 메일 디자인 변경

--- raonProject/src/main/java/com/raon/raondanim/service/AccountsService.java
+++ raonProject/src/main/java/com/raon/raondanim/service/AccountsService.java
@@ -411,15 +411,7 @@ public class AccountsService {
 		String name = user.getUser_lnm() + user.getUser_fnm(); //유저 이름을 담는 변수
 		
 		subject = "라온다님 회원가입 인증 메일입니다.";
-		msg += "<div align='center' style='border:1px solid black; font-family:verdana'>";
-		msg += "<h3 style='color: blue;'>";
-		msg += name + "님 회원가입을 환영합니다.</h3>";
-		msg += "<div style='font-size: 130%'>";
-		msg += "하단의 인증 버튼 클릭 시 정상적으로 회원가입이 완료됩니다.</div><br/>";
-		msg += "<form method='post' action='http://localhost:8081/accounts/certify'>";
-		msg += "<input type='hidden' name='user_id' value='" + user.getUser_id() + "'>";
-		msg += "<input type='hidden' name='user_verify_code' value='" + user.getUser_verify_code() + "'>";
-		msg += "<input type='submit' value='인증'></form><br/></div>";
+		msg += "<table border='0'cellpadding='0'cellspacing='0'width='100%'style='table-layout: fixed;'><tr><td bgcolor='#ffffff'align='center'style='padding: 70px 15px 70px 15px;'class='section-padding'><table border='0'cellpadding='0'cellspacing='0'width='500'class='responsive-table'><tr><td><table width='100%'border='0'cellspacing='0'cellpadding='0'><tr><td><table width='100%'border='0'cellspacing='0'cellpadding='0'><tbody><tr><td class='padding-copy'><table width='100%'border='0'cellspacing='0'cellpadding='0'><tr><td><a target='_blank'><img src='https://i.imgur.com/JhhrjFD.jpg'width='500'height='200'border='0'alt='Can an email really be responsive?'style='display: block; padding: 0; color: #666666; text-decoration: none; font-family: Helvetica, arial, sans-serif; font-size: 16px; width: 500px; height: 200px;'class='img-max'></a></td></tr></table></td></tr></tbody></table></td></tr><tr><td><table width='100%'border='0'cellspacing='0'cellpadding='0'><tr><td align='center'style='font-size: 25px; font-family: Helvetica, Arial, sans-serif; color: #333333; padding-top: 30px;'class='padding-copy'><br><br>"+name+"님 라온다님 회원가입을 환영합니다.</td></tr><tr><td align='center'style='padding: 20px 0 0 0; font-size: 16px; line-height: 25px; font-family: Helvetica, Arial, sans-serif; color: #666666;'class='padding-copy'>하단의 인증 버튼 클릭 시<br>정상적으로 회원가입이 완료됩니다.<br></td></tr></table></td></tr><tr><td><table width='100%'border='0'cellspacing='0'cellpadding='0'class='mobile-button-container'><tr><td align='center'style='padding: 25px 0 0 0;'class='padding-copy'><table border='0'cellspacing='0'cellpadding='0'class='responsive-table'></table></td></tr></table></td></tr></table></td></tr></table></td></tr></table><div style='width: 100%; text-align:center;'><div style='margin: 0 auto; '><form method='post'action='http://localhost:8081/accounts/certify'><input type='hidden'name='user_id'value='" + user.getUser_id() + "'><input type='hidden'name='user_verify_code'value='" + user.getUser_verify_code() + "'><input type='submit'value='인증'style='font-size: 16px; font-family: Helvetica, Arial, sans-serif; font-weight: normal; color: #ffffff; text-decoration: none; background-color: #5D9CEC; border-top: 15px solid #5D9CEC; border-bottom: 15px solid #5D9CEC; border-left: 25px solid #5D9CEC; border-right: 25px solid #5D9CEC; border-radius: 3px; -webkit-border-radius: 3px; -moz-border-radius: 3px; display: inline-block;'class='mobile-button'></form></div></div>";
 
 		// 받는 사람 E-Mail 주소
 		String mail = user.getUser_id();
